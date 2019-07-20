b4e88bddcf61599c62b95c27c0a0b449eb598677
Suresh Prasanna <sureshprasanna70@gmail.com>
2019-07-20 22:32:09 +0530

    add timer per question basis

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func check(err error) {
@@ -18,14 +20,17 @@ func check(err error) {
 type problem struct {
 	question string
 	answer   string
+	tLimit   int
 }
 
 func parseLines(lines [][]string) []problem {
 	probStruct := make([]problem, len(lines))
-	for i := range lines {
+	for i, line := range lines {
+		intTime, _ := strconv.Atoi(line[2])
 		probStruct[i] = problem{
-			question: lines[i][0],
-			answer:   strings.TrimSpace(lines[i][1]),
+			question: line[0],
+			answer:   strings.TrimSpace(line[1]),
+			tLimit:   intTime,
 		}
 	}
 	return probStruct
@@ -34,7 +39,10 @@ func parseLines(lines [][]string) []problem {
 func main() {
 	correct := 0
 	csvFilename := flag.String("csv", "ques.csv", "Location of questions in csv format")
+	//timeLimit := flag.Int("limit", 30, "Time limit to answer a question")
 	flag.Parse()
+	//parse the flags before using it
+	answerCh := make(chan string)
 	dat, err := os.Open(*csvFilename)
 	if err != nil {
 		fmt.Printf("failed to open %s\n", *csvFilename)
@@ -45,12 +53,23 @@ func main() {
 	lines, err := reader.ReadAll()
 	problems := parseLines(lines)
 	for i, p := range problems {
-		fmt.Printf("Question #%d : %s \n", i+1, p.question)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.answer {
-			correct = correct + 1
+		fmt.Printf("\nYou have got %d seconds to answer this question", p.tLimit)
+		timer := time.NewTimer(time.Duration(p.tLimit) * time.Second)
+		fmt.Printf("\nQuestion #%d : %s =", i+1, p.question)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+		select {
+		case <-timer.C:
+			continue
+		case answer := <-answerCh:
+			if answer == p.answer {
+				correct = correct + 1
+			}
 		}
+
 	}
 	fmt.Printf("You got %d out of %d\n", correct, len(problems))
 }
